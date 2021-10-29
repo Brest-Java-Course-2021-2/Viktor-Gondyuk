421be33cb3c9c89939fd69ffb6f92f12a99ae954
viktor <vik.gondyuk@gmail.com>
2021-10-29 20:16:03 +0300

    2d

--- src/main/java/com/epam/brest/Main.java
+++ src/main/java/com/epam/brest/Main.java
@@ -1,7 +1,40 @@
 package com.epam.brest;
 
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Privet!");
+        //result = m * pricePerKg + km * pricePerKm;
+        //
+        //
+
+        BigDecimal weight, pricePerKg, length, pricePerKm;
+        
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (!scanner.hasNext("q")) {
+
+                weight = getValueCon(scanner, "Enter weight:");
+
+                pricePerKg = getValueCon(scanner, "Enter pricePerKg");
+
+                length = getValueCon(scanner, "Enter leght:");
+
+                pricePerKm = getValueCon(scanner, "Enter pricePerKm");
+
+                BigDecimal result = weight.multiply(pricePerKg).add(length.multiply(pricePerKm));
+                System.out.println("Result:" + result);
+            }
+        }
+
     }
+
+    private static BigDecimal getValueCon(Scanner scanner, String s) {
+        BigDecimal enteredValue;
+        System.out.print("Enter m:");
+        enteredValue = scanner.nextBigDecimal();
+        System.out.println("Enter value:" + enteredValue);
+        return enteredValue;
+    }
+
 }
